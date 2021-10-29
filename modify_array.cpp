0a30d0e6f3c867ed0410da3573e599e50ca4b89a
Chi Truong <vtchitruong@gmail.com>
2021-10-29 11:06:54 +0700

    Simple tasks to modify array

--- modify_array.cpp
+++ modify_array.cpp
@@ -3,8 +3,12 @@
 
 using namespace std;
 
-void show(int a[], int n)
+int a[100];
+int n; // size of array
+
+void show(string msg) // message
 {
+    cout << msg << endl;
     for (int i = 0; i < n; i++)
     {
         cout << a[i] << ' ';
@@ -12,22 +16,69 @@ void show(int a[], int n)
     cout << endl;
 }
 
+//--------------------------
+void insert(int value, int pos)
+{
+    n++; // increase size
 
+    if (pos < n)
+    {
+        // shift elements 1-step backward
+        for (int i = n - 1; i >= pos; i--)
+        {
+            a[i + 1] = a[i];
+        }
+
+        // insert the new element
+        a[pos] = value;
+    }
+    else
+    {
+        cout << "Invalid position" << endl;
+    }
+}
+
+//--------------------------
+void append(int value)
+{   
+    a[n] = value;
+    n++; // increase size
+}
+
+//--------------------------
 
 
 int main()
-{
-    int myArray[100];
-    int size = 10;
-
+{ 
     // init array
-    for (int i = 0; i < size; i++)
+    n = 10;
+    for (int i = 0; i < n; i++)
     {
-        myArray[i] = rand() % 10;
+        a[i] = rand() % 10;
     }
 
-    cout << "Initial array" << endl;
-    show(myArray, size);
+    // show
+    show("Initial array");
 
+    //--------------------------
+    // insert to position 6
+    int newElement = 77;
+    int position = 6;
+    insert(newElement, position);
+    show("Insert to some position");
+
+    //--------------------------
+    // insert to position 6
+    newElement = 77;
+    position = 0;
+    insert(newElement, position);
+    show("Insert to the head");
+
+    //--------------------------
+    // append
+    newElement = 88;
+    append(newElement);
+    show("Add a new element to the tail");
+    
     return 0;
 }
